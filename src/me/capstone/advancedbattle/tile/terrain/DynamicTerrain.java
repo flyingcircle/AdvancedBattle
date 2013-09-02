c199dd68f8f77231e84ddcc196bc40e3e40e444c
MCManCub <mcmancub@gmail.com>
2013-09-01 23:39:45 -0500

    This should also be abstract since it isn't "surface level"

--- src/me/capstone/advancedbattle/tile/terrain/DynamicTerrain.java
+++ src/me/capstone/advancedbattle/tile/terrain/DynamicTerrain.java
@@ -3,7 +3,7 @@ package me.capstone.advancedbattle.tile.terrain;
 import me.capstone.advancedbattle.tile.TileType;
 
 // DynamicTerrain represents a terrain tile that can change states (neutral, own, states of capture, etc.)
-public class DynamicTerrain extends Terrain {
+public abstract class DynamicTerrain extends Terrain {
 
 	protected DynamicTerrain(int row, int column, TileType type) {
 		super(row, column, type);
