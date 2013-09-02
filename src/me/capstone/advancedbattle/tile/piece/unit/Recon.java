0b98240c852d138e866dd655feb47b5afe94089d
MCManCub <mcmancub@gmail.com>
2013-09-01 23:38:58 -0500

    Change information scope to public on "surface level" classes

--- src/me/capstone/advancedbattle/tile/piece/unit/Recon.java
+++ src/me/capstone/advancedbattle/tile/piece/unit/Recon.java
@@ -6,7 +6,7 @@ import me.capstone.advancedbattle.tile.piece.Piece;
 //Class for the Recon piece. We will put health, fuel, damage, movement speed, and sight in here.
 public class Recon extends Piece {
 
-	protected Recon(int row, int column) {
+	public Recon(int row, int column) {
 		super(row, column, TileType.RECON);
 	}
 }
