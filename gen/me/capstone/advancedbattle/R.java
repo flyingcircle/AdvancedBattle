cdb45c05ba92e2b83a5bb18ffa64d4ade5c6acff
MCManCub <mcmancub@gmail.com>
2013-11-15 16:42:41 -0600

    Remove potential menu stuff. Using AndEngine.

--- gen/me/capstone/advancedbattle/R.java
+++ gen/me/capstone/advancedbattle/R.java
@@ -25,32 +25,25 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int Screen=0x7f080001;
-        public static final int end_turn=0x7f08000a;
-        public static final int fragment_container=0x7f080000;
-        public static final int options=0x7f08000b;
-        public static final int sign_in_bar=0x7f080002;
-        public static final int sign_in_button=0x7f080003;
-        public static final int sign_in_text=0x7f080004;
-        public static final int sign_out_bar=0x7f080005;
-        public static final int sign_out_button=0x7f080006;
-        public static final int sign_out_text=0x7f080007;
-        public static final int start_bar=0x7f080008;
-        public static final int start_button=0x7f080009;
+        public static final int Screen=0x7f070001;
+        public static final int fragment_container=0x7f070000;
+        public static final int sign_in_bar=0x7f070002;
+        public static final int sign_in_button=0x7f070003;
+        public static final int sign_in_text=0x7f070004;
+        public static final int sign_out_bar=0x7f070005;
+        public static final int sign_out_button=0x7f070006;
+        public static final int sign_out_text=0x7f070007;
+        public static final int start_bar=0x7f070008;
+        public static final int start_button=0x7f070009;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int fragment_mainmenu=0x7f030001;
     }
-    public static final class menu {
-        public static final int game_menu=0x7f070000;
-    }
     public static final class string {
         public static final int app_id=0x7f050000;
         public static final int app_name=0x7f050001;
         public static final int background=0x7f050002;
-        public static final int end_turn=0x7f050007;
-        public static final int options=0x7f050008;
         public static final int sign_in_text=0x7f050003;
         public static final int sign_out_button=0x7f050004;
         public static final int sign_out_text=0x7f050005;
